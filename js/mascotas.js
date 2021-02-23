b988f05b49382edf7493f4faadbabe1967f3f49a
Mario López <mario_angel99@hotmail.com>
2021-02-23 16:32:23 -0600

    Se agregó la función de agregas mascotas + editar mascota

--- js/mascotas.js
+++ js/mascotas.js
@@ -1,6 +1,7 @@
 const tipo = document.getElementById('tipo');
 const nombre = document.getElementById('nombre');
-const dueño = document.getElementById('dueño');
+const dueno = document.getElementById('dueno');
+const indice = document.getElementById('indice');
 const form = document.getElementById('formulario');
 const btnGuardar = document.getElementById('guardar');
 const listaMascotas = document.getElementById('lista-mascotas');
@@ -9,35 +10,42 @@ const listaMascotas = document.getElementById('lista-mascotas');
 let mascotas = [
     {tipo: "Gato",
     nombre: "manchas",
-    dueño: "Mario"
+    dueno: "Mario"
     }
 ];
 
 function listarMascotas()
 {
-    const htmlMascotas = mascotas.map((mascota,indice)=>`<tr>
-        <th scope="row">${indice}</th>
+    const htmlMascotas = mascotas.map((mascota,index)=>`<tr>
+        <th scope="row">${index}</th>
         <td>${mascota.tipo}</td>
         <td>${mascota.nombre}</td>
-        <td>${mascota.dueño}</td>
-        <td><button class="btn btn-success"><i class="fas fa-edit"></i></button>
+        <td>${mascota.dueno}</td>
+        <td><button class="btn btn-success editar" data-indice?${index}><i class="fas fa-edit"></i></button>
         <button class="btn btn-danger"><i class="far fa-trash-alt"></i></button>
         </td>
     </tr>`).join("");
     listaMascotas.innerHTML=htmlMascotas;
+        Array.from(document.getElementsByClassName('editar')).forEach((botonEditar)=>botonEditar.onclick=editar)
 }
-function enviarDatos(evento)
+function enviarDatos(e)
 {
-    evento.PreventDefault();
+    // e.PreventDefault();
     const datos = 
     {
         tipo: tipo.value,
         nombre: nombre.value,
-        dueño: dueño.value
+        dueno: dueno.value
     };
     mascotas.push(datos);
     listarMascotas();
 }
+function editar(e)
+{
+    console.log('editar' , e)
+}
+
+
 listarMascotas();
 
 form.onsubmit = enviarDatos;
