618f1d612e99c2c5d7532f2ce4b78c9898bd31c5
Mario López <mario_angel99@hotmail.com>
2021-02-24 17:09:53 -0600

    Añadido función de editar y eliminar, con los plus indicados del profe (pequeño detalle en el botón de Eliminar)

--- js/mascotas.js
+++ js/mascotas.js
@@ -5,12 +5,17 @@ const indice = document.getElementById('indice');
 const form = document.getElementById('formulario');
 const btnGuardar = document.getElementById('guardar');
 const listaMascotas = document.getElementById('lista-mascotas');
+const titulo = document.getElementById('label');
 
 
 let mascotas = [
     {tipo: "Gato",
     nombre: "manchas",
     dueno: "Mario"
+    },
+    {tipo: "Perro",
+    nombre: "Pluto",
+    dueno: "Angel"
     }
 ];
 
@@ -21,32 +26,76 @@ function listarMascotas()
         <td>${mascota.tipo}</td>
         <td>${mascota.nombre}</td>
         <td>${mascota.dueno}</td>
-        <td><button class="btn btn-success editar" data-indice?${index}><i class="fas fa-edit"></i></button>
-        <button class="btn btn-danger"><i class="far fa-trash-alt"></i></button>
+        <td><button class="btn btn-success editar" data-bs-toggle="modal" data-bs-target="#exampleModal"><i class="fas fa-edit"></i></button>
+        <button class="btn btn-danger eliminar" data-bs-toggle="modal" data-bs-target="#modalEliminar" data-bs-whatever="@mdo"><i class="far fa-trash-alt"></i></button>
         </td>
     </tr>`).join("");
     listaMascotas.innerHTML=htmlMascotas;
-        Array.from(document.getElementsByClassName('editar')).forEach((botonEditar)=>botonEditar.onclick=editar)
+    Array.from(document.getElementsByClassName('editar')).forEach((botonEditar,index)=>botonEditar.onclick=editar(index))
+    Array.from(document.getElementsByClassName('eliminar')).forEach((botonEliminar,index)=>botonEliminar.onclick=eliminar(index))
 }
 function enviarDatos(e)
 {
-    // e.PreventDefault();
     const datos = 
     {
         tipo: tipo.value,
         nombre: nombre.value,
         dueno: dueno.value
     };
-    mascotas.push(datos);
+    const accion = btnGuardar.innerHTML;
+    switch(accion)
+    {
+        case 'Editar':
+            mascotas[indice.value] = datos;
+            break;
+        default:
+            mascotas.push(datos);
+            break;
+    }
+    listarMascotas();
+    resetModal();
+}
+function editar(index)
+{
+    return function cuandoCliqueo()
+    {
+        titulo.innerHTML='Editar mascota';
+        btnGuardar.innerHTML='Editar';
+
+        const mascota = mascotas[index];
+        nombre.value = mascota.nombre;
+        dueno.value = mascota.dueno;
+        tipo.value = mascota.tipo;
+        indice.value = index;
+    }
+}
+
+function resetModal()
+{
+    nombre.value = '';
+    dueno.value = '';
+    tipo.value = 'Tipo animal';
+    indice.value = '';
+    btnGuardar.innerHTML = 'Crear'
+}
+
+function eliminar(index)
+{
+    return function clickEliminar()
+    {
+        mascotas = mascotas.filter((mascota,indiceMascota)=>indiceMascota !== index);
+    }
+}
+function confirmarEliminacion()
+{
+    eliminar();
     listarMascotas();
 }
-function editar(e)
-{
-    console.log('editar' , e)
-}
-
-
 listarMascotas();
 
 form.onsubmit = enviarDatos;
 btnGuardar.onclick = enviarDatos;
+
+
+
+
